fc8409607d6ccea1865d0bd668fe21ad11e0859d
v ilin <v_ilin@ga-tech.co.jp>
2023-12-28 22:55:33 +0900

    wrong symbol

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -9,7 +9,7 @@ func Hello(name string) string {
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
 
-	message := fmt.Sprintf("Hi, $v. Welcome!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	
 	return message
 }
