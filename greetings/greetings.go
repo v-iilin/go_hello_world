bc3bbd3ed8e45c3f7e9644480a20afda414c453d
v ilin <v_ilin@ga-tech.co.jp>
2023-12-29 10:10:09 +0900

    handle errors

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,15 +1,25 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
 	// Return a greeting that embeds the name in a message.
 	
+	// if no name was given, return an error witha message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// if a name was received, return a value that embeds the name
+
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
 
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	
-	return message
+	return message, nil
 }
--- readme.md
+++ readme.md
@@ -4,5 +4,6 @@ go hello world tutorial from go.dev
 1. https://go.dev/doc/tutorial/getting-started
 2. https://go.dev/doc/tutorial/create-module
 3. https://go.dev/doc/tutorial/call-module-code
+4. https://go.dev/doc/tutorial/handle-errors
 ### current:
-1. https://go.dev/doc/tutorial/handle-errors
+1. https://go.dev/doc/tutorial/random-greeting
