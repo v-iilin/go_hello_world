6b37aee790b492f89444c0417624ff44609db3e6
v ilin <v_ilin@ga-tech.co.jp>
2023-12-30 23:12:43 +0900

    random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person.
@@ -19,7 +20,19 @@ func Hello(name string) (string, error) {
 	// var message string
 	// message = fmt.Sprintf("Hi, %v. Welcome!", name)
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	
 	return message, nil
+}
+
+func randomFormat() string {
+	// A slice of message formats
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}	
+	// return a randomly selected message format by specifying 
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
--- readme.md
+++ readme.md
@@ -5,5 +5,6 @@ go hello world tutorial from go.dev
 2. https://go.dev/doc/tutorial/create-module
 3. https://go.dev/doc/tutorial/call-module-code
 4. https://go.dev/doc/tutorial/handle-errors
+5. https://go.dev/doc/tutorial/random-greeting
 ### current:
-1. https://go.dev/doc/tutorial/random-greeting
+1. https://go.dev/doc/tutorial/greetings-multiple-people
